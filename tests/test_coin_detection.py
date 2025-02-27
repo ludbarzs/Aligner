aca7d98f3badc2922ef732e3762aa3901431990f
arvils <arvilslud1@gmail.com>
2025-02-27 15:28:17 +0200

    Added tests module Adde test_coin_detetction module

--- tests/test_coin_detection.py
+++ tests/test_coin_detection.py
@@ -1,7 +1,10 @@
 import cv2 as cv
 import numpy as np
 
-from src.image_processing.circularity import coin_top_left_corner, find_px_to_mm_ratio
+from src.image_processing.coin_detection import (
+    coin_top_left_corner,
+    find_px_to_mm_ratio,
+)
 
 
 def test_detect_coin_in_top_left(contours):
@@ -10,24 +13,19 @@ def test_detect_coin_in_top_left(contours):
 
 
 def test_find_px_to_mm_ratio(contours):
-    image = np.zeros((200, 200))
-    cv.circle(
-        image, (100, 100), 50, 255, -1
-    )  # image, center of circle, cirlce radius, circle colour, thickness (-1 filled)
-    controus, _ = cv.findContours(image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     coin_cointour = contours[0]
 
     # Euro coin diameter 23.25
     ratio = find_px_to_mm_ratio(coin_cointour)
     expected_ratio = 100 / 23.25
 
-    # Accuracy of 1%
+    # Accuracy of 1% expected
     assert (ratio - expected_ratio) < 0.01
 
 
 def test_no_coin_detected():
-    img = np.zeros((200, 200, 3))
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    img = np.zeros((200, 200, 3), dtype=np.uint8)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     edges = cv.Canny(gray, 50, 150)
     contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
