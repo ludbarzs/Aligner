0a8702c30ccf01d6459d4f71df7d4f209854095c
arvils <arvilslud1@gmail.com>
2025-03-27 15:58:26 +0200

    Added fuck knows what pc broke :(

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from file_conversion.dxf_exporter import contours_to_dxf
 from image_processing.coin_detection import (
     detect_circles,
@@ -9,23 +7,24 @@ from image_processing.drawer_detection import (
     draw_contour_line,
     find_inscribed_circle_diameter,
     measure_object_in_drawer,
+    select_drawer_corners,
 )
 from image_processing.utils import find_contours, load_image, prepare_image, view_image
 
 
 def main():
     # Load Image
-    image = load_image("images/test_14.jpg")
+    image = load_image("images/test_19.jpg")
 
     # User selects drawer corners
-    # corners = select_drawer_corners(image)
+    corners = select_drawer_corners(image)
 
     # Auto select corners for test_14.jpg
-    corners = np.array([[149, 82], [1939, 75], [1995, 1213], [115, 1240]])
+    # corners = np.array([[149, 82], [1939, 75], [1995, 1213], [115, 1240]])
 
     # Image perspective gets corrected
-    irl_width = 540
-    irl_length = 340
+    irl_width = 297
+    irl_length = 210
     corrected_image, x_ratio, y_ratio = correct_perspective(
         image, corners, irl_width, irl_length
     )
@@ -56,6 +55,7 @@ def main():
     for contour in contours:
         draw_contour_line(corrected_image, contour, x_ratio, y_ratio)
 
+    view_image(corrected_image, contours)
     # Allows user to measure
     measure_object_in_drawer(corrected_image, x_ratio, y_ratio)
 
