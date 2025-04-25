a3cf0db1170d393cf93b96f183ccad7311c2fd22
arvils <arvilslud1@gmail.com>
2025-04-25 12:17:24 +0300

    Fixed imports, added test image

--- src/core/image_processor.py
+++ src/core/image_processor.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
-from error.error import Error
+
+from errors.error import Error
 
 
 class ImageProcessor:
--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 import cv2 as cv
 import numpy as np
 
-# from core.image_processor import ImageProcessor
-from errors.error import Error
+from core.image_processor import ImageProcessor
 
 
 def main():
     """Main"""
-
-    Error.type("str", "dumdum")
 
     image_path = "images/test_1.jpg"
     ImageProcessor.load_image(image_path)
